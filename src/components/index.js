80542a153dd05e9966c167fe3170599c7b2f4e23
Chukwumdiebube <chukwumdiebubeojinta@gmail.com>
2023-01-17 20:28:29 +0100

    added styles and routes

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+import Feed from "./Feed";
+import SearchFeed from "./SearchFeed";
+import ChannelDetail from "./ChannelDetail";
+import VideoDetail from "./VideoDetail";
+
+export {
+    Navbar, SearchFeed, ChannelDetail, VideoDetail, Feed
+}
